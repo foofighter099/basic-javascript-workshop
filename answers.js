8f91ef2abe449d44dd98c660abfa0679e050e529
foofighter0991 <andrewtognarini@hotmail.com>
2016-01-27 20:40:09 +0000

    string & number

--- answers.js
+++ answers.js
@@ -1,5 +1,5 @@
-// this is to return the last letter of a string
-function lastLetter(str) {
-    return str.slice(str.length-1);
+// this is to return the character at the position represented by the number
+var numChar = function(string,number) {
+    return string.substr((parseInt(number)-1),1);
 }
-console.log(lastLetter("Andrew"))
+console.log(numChar("abcdefghijklmnopqrstuvwxyz",20))
