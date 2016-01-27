9f4db2347245809cf2b8cfe4e85db5a80e06273c
foofighter0991 <andrewtognarini@hotmail.com>
2016-01-27 22:34:42 +0000

    longest word

--- answers.js
+++ answers.js
@@ -1,14 +1,14 @@
-// this is to reverse the characters in the string
-function factorial(num){
-    if (num < 0) {
-        return -1;
-    }else if (num == 0) {
-        return 1;
-    }
-    var tmp = num;
-    while (num-- > 2) {
-        tmp *= num;
-    }
-    return tmp;
+// this is to return the longest word in the string
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    str.forEach(function(str) {
+        if (longest < str.length) {
+            longest = str.length;
+            word = str;
+        }
+    });
+    return word;
 }
-console.log(factorial(8))
+console.log(longestWord("samsung series laptop cadillac"));
