13585c8cb97564cb020e5c154a65603e70dad654
foofighter0991 <andrewtognarini@hotmail.com>
2016-01-27 20:58:29 +0000

    multiply two numbers

--- answers.js
+++ answers.js
@@ -1,6 +1,6 @@
-// this is to return the addition of the two numbers
-var numAdd = function(num1,num2){
-    return(parseInt(num1)+parseInt(num2));
+// this is to return the sum of the two numbers
+var numSum = function(num1,num2){
+    return(parseInt(num1)*parseInt(num2));
 }
-console.log(numAdd(5,10))
+console.log(numSum(5,10))
 //if we input something other than a number (ex:"jar") we get an output of: NaN (not a number)
