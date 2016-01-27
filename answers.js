7e85fd8b5a6d075c095e2b3286d0dc6c89282a0a
foofighter0991 <andrewtognarini@hotmail.com>
2016-01-27 21:45:02 +0000

    repeat string

--- answers.js
+++ answers.js
@@ -1,23 +1,6 @@
-// this is to return the sum of the two numbers
-var twoNum = function (num1,num2){
-var answer = prompt ("would you like to add, subtract, mult, or div?");
-    switch(answer){
-        case 'add':
-            alert(parseInt(num1)+parseInt(num2));
-            break;
-        case 'subtract':
-            alert(parseInt(num1)-parseInt(num2));
-            break;
-        case 'mult':
-            alert(parseInt(num1)*parseInt(num2));
-            break;
-        case 'div':
-            alert(parseInt(num1)/parseInt(num2));
-            break;
-        default:
-            alert(0);
-            break;
-    }
+// this is to repeat the string 'n' number of times 
+function repeat(str, times) {
+    return new Array(times + 1).join(str);
 }
-twoNum(5,4)
+console.log(repeat("WOOT",7))
    
